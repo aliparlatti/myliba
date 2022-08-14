82b927cc4fe40f92fb301d7e1f2cde30ed02408d
aliparlatti <aliparlatti@gmail.com>
2022-08-14 15:21:36 +0300

    Register v1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,89 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'myliba_register';
+  title = 'MyLiba Register';
+  public users: any = [];
+  public statusUpdate: boolean = false;
+  public user_id: string = '';
+  getAllUsers() {
+
+    this.db.collection('users').snapshotChanges().subscribe((response) => {
+      this.users = response.map(item =>
+        Object.assign({ id: item.payload.doc.id }, item.payload.doc.data()),
+      );
+    })
+  }
+  onUpdate(id: string) {
+    this.statusUpdate = false;
+    console.log(id);
+
+    const name = this.registerForm.controls['name'].value;
+    const mail = this.registerForm.controls['mail'].value;
+    const password = this.registerForm.controls['pass'].value;
+    let data = {
+      'name': name,
+      'mail': mail,
+      'pass': password
+    };
+    this.db.collection('users').doc(id).set(data);
+    this.clearForm();
+  }
+  onDelete(id: string) {
+    this.db.collection('users').doc(id).delete();
+  }
+  onEdit(id: string) {
+    this.user_id = id;
+    this.statusUpdate = true;
+    let user = this.users.find((x: { id: string; }) => x.id === id);
+    this.registerForm = this.formBuilder.group({
+      name: user.name,
+      mail: user.mail,
+      pass: user.pass
+    });
+  }
+  ngOnInit(): void {
+    this.clearForm();
+    this.getAllUsers();
+  }
+  clearForm() {
+    this.registerForm = this.formBuilder.group({
+      name: '',
+      mail: '',
+      pass: ''
+    });
+  }
+  registerForm = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+    mail: new FormControl('', [Validators.required]),
+    pass: new FormControl('', [Validators.required])
+  });
+  constructor(private formBuilder: FormBuilder,
+    private db: AngularFirestore) {
+
+  }
+  onSubmit() {
+    const name = this.registerForm.controls['name'].value;
+    const mail = this.registerForm.controls['mail'].value;
+    const password = this.registerForm.controls['pass'].value;
+    let data = {
+      'name': name,
+      'mail': mail,
+      'pass': password
+    };
+    this.db.collection('users').add(data);
+    this.clearForm();
+  }
+
+  get rf() {
+    return this.registerForm.controls;
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,35 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from "@angular/fire/compat/firestore";
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBSWY0IMsX3N0Q-n22pkZG0AwePCDLj9rM",
+  authDomain: "myliba-register.firebaseapp.com",
+  projectId: "myliba-register",
+  storageBucket: "myliba-register.appspot.com",
+  messagingSenderId: "1079857849506",
+  appId: "1:1079857849506:web:e09ebf4418aa0291ede3fb"
+};
 
 @NgModule({
   declarations: [
     AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
+  ], 
+  imports: [ 
+    BrowserModule, 
+    AppRoutingModule,
+    NgbModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+ 
